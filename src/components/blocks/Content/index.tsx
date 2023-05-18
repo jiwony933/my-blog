26d8f333d5a24809836e7ed80796be805aaed606
jiwony933 <jiwony933@naver.com>
2023-05-19 03:03:43 +0900

    패딩 조정..

--- src/components/blocks/Content/index.tsx
+++ src/components/blocks/Content/index.tsx
@@ -26,17 +26,17 @@ import MarkdownViewer from './MarkDownViewer';
 export const Container = styled(FlexColumnBox)<{ isMobile?: boolean }>`
   gap: 12px;
   padding-bottom: 200px;
-  padding-left: 50px;
-  padding-right: 50px;
+  padding-left: 20px;
+  padding-right: 20px;
 `;
 
 export const Title = styled.div<{ isMobile?: boolean }>`
-  font-size: ${({ isMobile }) => (isMobile ? '32px' : '44px')};
+  font-size: ${({ isMobile }) => (isMobile ? '30px' : '44px')};
   font-weight: 600;
 `;
 export const Date = styled(FlexEndBox)<{ isMobile?: boolean }>`
   font-weight: 400;
   color: var(--grey700);
   font-size: ${({ isMobile }) => (isMobile ? '14px' : '18px')};
-  margin-bottom: 80px;
+  margin-bottom: ${({ isMobile }) => (isMobile ? '20px' : '40px')};
 `;
